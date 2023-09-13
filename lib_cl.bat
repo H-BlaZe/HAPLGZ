aba9fd9f0e8fa6b2917a6400e06e0c8a2086933a
ibnAdam <100560353+H-BlaZe@users.noreply.github.com>
2023-09-14 01:09:01 +0530

    Add files via upload

---
+++ lib_cl.bat
@@ -0,0 +1,14 @@
+c:\
+pip install pytransform
+pip install pyarmour
+pip install os
+pip install re
+pip install time
+pip install pyfiglet
+pip install requests
+pip install googlesearch
+pip install googlesearch-python
+pip install bs4
+pip install colorama
+echo CLOSE THE WINDOW AND RUN PY FILE
+pause
